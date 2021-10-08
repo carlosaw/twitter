702e298c241c126cd25502c88bf74cadfc1afc2f
Carlos Alberto <carlosfreevg@gmail.com>
2021-10-08 10:20:08 -0400

    Verify-OK

--- twitter.sql
+++ twitter.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Out-2021 às 03:21
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 7.4.13
+-- Tempo de geração: 08-Out-2021 às 16:19
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 7.4.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,16 @@ CREATE TABLE `posts` (
   `mensagem` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`id`, `id_usuario`, `data_post`, `mensagem`) VALUES
+(1, 1, '2021-10-08 09:53:43', 'Esta é uma mensagem de teste.'),
+(2, 1, '2021-10-08 10:06:28', 'Teste legal...'),
+(3, 5, '2021-10-08 10:07:00', 'Olá Este é um post de Aninha.'),
+(4, 3, '2021-10-08 10:11:01', 'Postagem de Bonieky');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +55,18 @@ CREATE TABLE `relacionamentos` (
   `id_seguidor` int(11) NOT NULL DEFAULT 0,
   `id_seguido` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `relacionamentos`
+--
+
+INSERT INTO `relacionamentos` (`id`, `id_seguidor`, `id_seguido`) VALUES
+(2, 1, 3),
+(4, 5, 1),
+(5, 4, 1),
+(8, 1, 5),
+(9, 5, 3),
+(10, 3, 5);
 
 -- --------------------------------------------------------
 
@@ -64,7 +86,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'Carlos', 'carlos@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
+(1, 'Carlos', 'carlos@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(2, 'Chico', 'chico@hotmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(3, 'Bonieky', 'boni@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(4, 'Mariana', 'mari@hotmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(5, 'Ana Paula', 'ana@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
 --
 -- Índices para tabelas despejadas
@@ -96,19 +122,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `relacionamentos`
 --
 ALTER TABLE `relacionamentos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
